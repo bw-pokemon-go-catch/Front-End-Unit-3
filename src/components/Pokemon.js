755342bd26e8dc129097b4e85ad0ba3a0028659f
Jason Aviles <sandman2k18@outlook.com>
2019-07-30 00:00:24 -0400

    styled cards

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -5,14 +5,22 @@ const Pokemon = (props) => {
     const {Name,PokemonNo,Type1,Type2,MaxHP,MaxCP,ImageURL}=props.poke
     return (
         <div style={{borderRadius:'10px',display:'flex',background:'white',margin:'20px',justifyContent:'center',alignItems:'center',flexDirection:'column',borderBottom:"2px #ff0000 solid",borderLeft:"1px #ff0000 solid",width:' 35%',
-        height: '212px'}}>
-        <div style={{border:'orange 5px solid',borderRadius:'10px',padding: '14px'}}>
+        height: 'auto'}}>
+        <div style={{border:'black 5px solid',borderRadius:'10px',padding: '14px'}}>
          <button style={{background:'transparent ' ,alignSelf:"end",padding:'none', outline:'none',border:'none'}} onClick={()=>props.DeletePokemon(props.poke.id)}>X</button>
-         <img style={{border:'groove 8px yellow',width:'76%',padding:'10px 20px' ,height:"137px"}} src={ImageURL}/>
-            <h1 style={{fontSize:'.8rem'}}>{Name}</h1>
-            <div>
-                <span>{Type1}</span>
-                <span>{Type2}</span>
+         <div  style={{display:'flex'  , justifyContent:'space-between'}}>
+         <h1 style={{fontSize:'.8rem',textAlign:' start',fontWeight:'bold'}}>{Name}</h1>
+         <h1 style={{fontSize:'.8rem',fontWeight:'bold'}}>Hp:{MaxHP}</h1></div>
+         <img style={{border:'groove 8px #ff0000',width:'76%',padding:'10px 20px' ,height:"137px",background:'#f3f1e1'}} src={ImageURL}/>
+            
+            <div style={{display:'flex' ,justifyContent:'space-evenly' ,fontSize:'.7rem',alignItems:'center'}}>
+                <span>type1: {Type1}</span>
+                <span>type2: {Type2}</span>
+            </div>
+            <div style={{display:'flex' ,justifyContent:'space-evenly',fontSize:'.5rem',alignItems:'center'}}>
+
+            <span>Cp:{MaxCP}</span>
+                <span>#{PokemonNo}</span>
             </div>
            </div>
         </div>
